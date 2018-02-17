2f25a3e335cce5cfb6c04adae76a9ce02960ebcf
KuanYuLai <yorklai0523@gmail.com>
2018-02-17 15:14:37 -0800

    add server file

---
+++ Server/server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain',
+        'Access-Control-Allow-Origin' : '*'
+    });
+    res.end('Hello World\n');
+}).listen(3000);
