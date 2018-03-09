c07241e09b617f342c856ba4918b199c16aac8cb
wrighch3 <wrighch3@oregonstate.edu>
2018-03-08 16:44:03 -0800

    WIP

---
+++ Zombie Project/connect_sql.php
@@ -0,0 +1,28 @@
+<?php
+
+
+// You will need to enter your own credentials here.
+// I provided the link to the database you will need to create in the Announcements on Canvas.
+// You can either make your own with the correct tables or ask me for a copy of it. 
+
+$servername = '';
+$dbname = '';
+$username = '';
+$password = '';
+
+
+$conn = new PDO("mysql:host=$servername; dbname=$dbname; charset=utf8", $username, $password, 
+            [PDO::ATTR_EMULATE_PREPARES => false, 
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+try {
+    //$conn = new PDO("mysql:host=$servername; dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+   // $conn = null;
+?>
