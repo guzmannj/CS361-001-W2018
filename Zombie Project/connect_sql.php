84ee9bace332db9428de051baba08ee3e438e4ac
CodePanoply <conundrumparadox33@gmail.com>
2018-03-12 18:59:57 -0700

    Adds nutrition info

--- Zombie Project/connect_sql.php
+++ Zombie Project/connect_sql.php
@@ -3,10 +3,6 @@
 // I provided the link to the database you will need to create in the Announcements on Canvas.
 // You can either make your own with the correct tables or ask me for a copy of it. 
 
-$servername = 'oniddb.cws.oregonstate.edu';
-$dbname = 'wrighch3-db';
-$username = 'wrighch3-db';
-$password = 'A2OQRIFXnnzmr3E0';
 
 $conn = new PDO("mysql:host=$servername; dbname=$dbname; charset=utf8", $username, $password, 
             [PDO::ATTR_EMULATE_PREPARES => false, 
